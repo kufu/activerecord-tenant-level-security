994042045a6843d9785f907b0ffb0cd1881b9408
wata727 <watassbass@gmail.com>
2019-12-26 14:10:53 +0900

    :see_no_evil:

--- lib/activerecord-tenant-level-security.rb
+++ lib/activerecord-tenant-level-security.rb
@@ -4,15 +4,12 @@ require 'pg'
 
 require_relative 'activerecord-tenant-level-security/tenant_level_security'
 require_relative 'activerecord-tenant-level-security/migration_extensions'
+require_relative 'activerecord-tenant-level-security/sidekiq'
 
 ActiveSupport.on_load(:active_record) do
   ActiveRecord::Migration.include TenantLevelSecurity::MigrationExtensions
 
   ActiveRecord::ConnectionAdapters::AbstractAdapter.set_callback :checkout, :after do |conn|
     TenantLevelSecurity.switch_with_connection!(conn, TenantLevelSecurity.current_tenant_id)
-  end
-
-  ActiveRecord::ConnectionAdapters::AbstractAdapter.set_callback :checkin, :before do |conn|
-    TenantLevelSecurity.switch_with_connection!(conn, nil)
   end
 end
--- lib/activerecord-tenant-level-security/sidekiq.rb
+++ lib/activerecord-tenant-level-security/sidekiq.rb
@@ -1,22 +1,26 @@
-module TenantLevelSecurity::Sidekiq::Middleware
-  class Client
-    def call(worker_class, job, queue, redis_pool)
-      if TenantLevelSecurity.current_session_tenant_id.present?
-        job['tenant_level_security'] ||= { id: TenantLevelSecurity.current_session_tenant_id }
-      end
+module TenantLevelSecurity
+  module Sidekiq
+    module Middleware
+      class Client
+        def call(worker_class, job, queue, redis_pool)
+          if TenantLevelSecurity.current_session_tenant_id.present?
+            job['tenant_level_security'] ||= { id: TenantLevelSecurity.current_session_tenant_id }
+          end
 
-      yield
-    end
-  end
-
-  class Server
-    def call(worker, job, queue)
-      if job.key?('tenant_level_security')
-        TenantLevelSecurity.with(job['tenant_level_security']['id']) do
           yield
         end
-      else
-        yield
+      end
+
+      class Server
+        def call(worker, job, queue)
+          if job.key?('tenant_level_security')
+            TenantLevelSecurity.with(job['tenant_level_security']['id']) do
+              yield
+            end
+          else
+            yield
+          end
+        end
       end
     end
   end
