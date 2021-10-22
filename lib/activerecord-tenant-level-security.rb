1a5ca6987cedec47b215d282c8be3ee43f8e9aab
meganemura <meganemura@users.noreply.github.com>
2021-10-22 09:11:40 +0900

    Rename MigrationExtensions to SchemaStatements

--- lib/activerecord-tenant-level-security.rb
+++ lib/activerecord-tenant-level-security.rb
@@ -3,11 +3,11 @@ require 'active_record'
 require 'pg'
 
 require_relative 'activerecord-tenant-level-security/tenant_level_security'
-require_relative 'activerecord-tenant-level-security/migration_extensions'
+require_relative 'activerecord-tenant-level-security/schema_statements'
 require_relative 'activerecord-tenant-level-security/sidekiq'
 
 ActiveSupport.on_load(:active_record) do
-  ActiveRecord::ConnectionAdapters::AbstractAdapter.include TenantLevelSecurity::MigrationExtensions
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.include TenantLevelSecurity::SchemaStatements
 
   ActiveRecord::ConnectionAdapters::AbstractAdapter.set_callback :checkout, :after do |conn|
     TenantLevelSecurity.switch_with_connection!(conn, TenantLevelSecurity.current_tenant_id)
