d616da4f3a11092dfabe48e7ee0adea5aae6951e
meganemura <meganemura@users.noreply.github.com>
2021-10-22 09:09:41 +0900

    Move schema statement definitions to ConnectionAdapter

--- lib/activerecord-tenant-level-security.rb
+++ lib/activerecord-tenant-level-security.rb
@@ -7,7 +7,7 @@ require_relative 'activerecord-tenant-level-security/migration_extensions'
 require_relative 'activerecord-tenant-level-security/sidekiq'
 
 ActiveSupport.on_load(:active_record) do
-  ActiveRecord::Migration.include TenantLevelSecurity::MigrationExtensions
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.include TenantLevelSecurity::MigrationExtensions
 
   ActiveRecord::ConnectionAdapters::AbstractAdapter.set_callback :checkout, :after do |conn|
     TenantLevelSecurity.switch_with_connection!(conn, TenantLevelSecurity.current_tenant_id)
