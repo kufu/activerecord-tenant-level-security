40989f89aaf581e3280f1e155072d8541a534023
shimx <shimura.32@gmail.com>
2024-07-31 11:01:47 +0200

    use quoted table and column names in create_policy and remove_policy methods

--- lib/activerecord-tenant-level-security/schema_statements.rb
+++ lib/activerecord-tenant-level-security/schema_statements.rb
@@ -1,28 +1,31 @@
 module TenantLevelSecurity
   module SchemaStatements
     def create_policy(table_name, partition_key: TenantLevelSecurity::DEFAULT_PARTITION_KEY)
+      quoted_table_name = quote_table_name(table_name)
+      quoted_partition_key = quote_column_name(partition_key)
       execute <<~SQL
-        ALTER TABLE #{table_name} ENABLE ROW LEVEL SECURITY;
-        ALTER TABLE #{table_name} FORCE ROW LEVEL SECURITY;
+        ALTER TABLE #{quoted_table_name} ENABLE ROW LEVEL SECURITY;
+        ALTER TABLE #{quoted_table_name} FORCE ROW LEVEL SECURITY;
       SQL
       tenant_id_data_type = get_tenant_id_data_type(table_name, partition_key)
       execute <<~SQL
-        CREATE POLICY tenant_policy ON #{table_name}
+        CREATE POLICY tenant_policy ON #{quoted_table_name}
           AS PERMISSIVE
           FOR ALL
           TO PUBLIC
-          USING (#{partition_key} = NULLIF(current_setting('tenant_level_security.tenant_id'), '')::#{tenant_id_data_type})
-          WITH CHECK (#{partition_key} = NULLIF(current_setting('tenant_level_security.tenant_id'), '')::#{tenant_id_data_type})
+          USING (#{quoted_partition_key} = NULLIF(current_setting('tenant_level_security.tenant_id'), '')::#{tenant_id_data_type})
+          WITH CHECK (#{quoted_partition_key} = NULLIF(current_setting('tenant_level_security.tenant_id'), '')::#{tenant_id_data_type})
       SQL
     end
 
     def remove_policy(table_name, *args)
+      quoted_table_name = quote_table_name(table_name)
       execute <<~SQL
-        ALTER TABLE #{table_name} NO FORCE ROW LEVEL SECURITY;
-        ALTER TABLE #{table_name} DISABLE ROW LEVEL SECURITY;
+        ALTER TABLE #{quoted_table_name} NO FORCE ROW LEVEL SECURITY;
+        ALTER TABLE #{quoted_table_name} DISABLE ROW LEVEL SECURITY;
       SQL
       execute <<~SQL
-        DROP POLICY tenant_policy ON #{table_name}
+        DROP POLICY tenant_policy ON #{quoted_table_name}
       SQL
     end
 
