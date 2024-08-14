1a06ee65a472be9b3c888d92b6ca114b65eb5d4c
mkmn <mkmn@users.noreply.github.com>
2024-08-14 12:41:40 +0900

    Add Comment for example of using ActiveSupport::CurrentAttribute.

--- lib/generators/tenant_level_security/templates/initializer.rb
+++ lib/generators/tenant_level_security/templates/initializer.rb
@@ -1,6 +1,14 @@
 # Pass current tenant ID to `TenantLevelSecurity.current_tenant_id` block
 # using ActiveSupport::CurrentAttributes, RequestStore, etc...
-# TenantLevelSecurity.current_tenant_id { RequestStore.store[:current_tenant_id] }
+#
+# The following example of using ActiveSupport::CurrentAttributes:
+#
+#   # app/models/current.rb
+#   class Current < ActiveSupport::CurrentAttributes
+#     attribute :current_tenant_id
+#   end
+#
+# TenantLevelSecurity.current_tenant_id { Current.curent_tenant_id }
 
 # Uncomment this line if you using activerecord-multi-tenant gem.
 # TenantLevelSecurity.current_tenant_id { MultiTenant.current_tenant_id }
